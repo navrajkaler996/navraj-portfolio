d3aacb967e14e5e380b387049df54e20f660f944
Navraj singh kaler <navrajkaler996@gmail.com>
2023-12-03 00:22:36 -0600

    footer fixed

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -4,10 +4,7 @@ const Footer: React.FC = () => {
   return (
     <div
       id="footer"
-      className="flex justify-center items-center h-[10rem] w-[100%] font-cuprum text-[#fff] mt-[5rem]"
-      style={{ background: "linear-gradient(to bottom, #0D1E31, #000)" }}>
-      <span>NAVRAJ SINGH KALER</span>
-    </div>
+      className="flex justify-center items-center h-[10rem] w-[100%] font-cuprum text-[#fff] mt-[5rem]"></div>
   );
 };
 
